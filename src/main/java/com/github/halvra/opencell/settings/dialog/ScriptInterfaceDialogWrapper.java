abc93aa1a3c467c35c8a5b82ad35c48380771395
Julien LEBOUTEILLER <julien.lebouteiller@gmail.com>
2023-04-07 23:59:53 +0200

    [feat] Add validation on dialogs

--- src/main/java/com/github/halvra/opencell/settings/dialog/ScriptInterfaceDialogWrapper.java
+++ src/main/java/com/github/halvra/opencell/settings/dialog/ScriptInterfaceDialogWrapper.java
@@ -2,9 +2,11 @@ package com.github.halvra.opencell.settings.dialog;
 
 import com.github.halvra.opencell.OpencellBundle;
 import com.intellij.openapi.ui.DialogWrapper;
+import com.intellij.openapi.ui.ValidationInfo;
 import com.intellij.ui.components.JBTextField;
 import com.intellij.util.ui.FormBuilder;
 import lombok.Getter;
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
@@ -40,5 +42,14 @@ public class ScriptInterfaceDialogWrapper extends DialogWrapper {
     protected void doOKAction() {
         this.value = scriptInterfaceField.getText();
         super.doOKAction();
+    }
+
+    @Override
+    protected @Nullable ValidationInfo doValidate() {
+        if (StringUtils.isBlank(scriptInterfaceField.getText())) {
+            return new ValidationInfo(OpencellBundle.message("settings.scriptInterfaces.required"), scriptInterfaceField);
+        }
+
+        return null;
     }
 }
